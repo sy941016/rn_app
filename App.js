f85cf3834c14bf57bb59291a5ed6ffe3ed8832bb
shiyuan <sybbb@qq.com>
2020-09-23 15:27:25 +0800

    react-navigation5x

--- App.js
+++ App.js
@@ -1,88 +1,143 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+// 4x
+// import { createBottomTabNavigator,createStackNavigator, createAppContainer } from 'react-navigation';//导航
+// 5x
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons'; //icon
-import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';//导航
+
 
 import HomeScreen from "./src/HomeScreen"
 import MyScreen from "./src/MyScreen"
 import NewsScreen from "./src/NewsScreen"
 import FindScreen from "./src/FindScreen"
 
-import page1 from "./src/page/page1"
-import page2 from "./src/page/page2"
-import page3 from "./src/page/page3"
+import Page1 from "./src/page/page1"
+import Page2 from "./src/page/page2"
+import Page3 from "./src/page/page3"
 
-const TabNavigator = createBottomTabNavigator(
-    {
-        "首页": HomeScreen,
-        "消息": NewsScreen,
-        "发现": FindScreen,
-        "我的": MyScreen
-    },
-    {
-        defaultNavigationOptions: ({ navigation }) => ({
-            tabBarIcon: ({ focused, horizontal, tintColor }) => {
-                const { routeName } = navigation.state;
-                let IconComponent = Ionicons;
-                let iconName;
-                if (routeName === '首页') {
-                    iconName = `ios-home`;
-                } else if (routeName === '消息') {
-                    iconName = `md-information-circle`;
-                } else if (routeName === '我的') {
-                    iconName = `ios-man`;
-                } else {
-                    iconName = `ios-folder`;
-                }
 
-                return <IconComponent name={iconName} size={25} color={tintColor} />;
-            },
-        }),
-        tabBarOptions: {
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray'
-        },
-        initialRouteName: '首页'
-    }
-);
+const Tab = createBottomTabNavigator();
 
-const AppStack = createStackNavigator(
-    {
-        TabNavigator: {
-            screen: TabNavigator,
-            navigationOptions: {
-                header: null
-            }
-        },
-        page1: {
-            screen: page1,
-            navigationOptions: {
-                title: "page1"
-            }
-        },
-        page2: {
-            screen: page2,
-            navigationOptions: {
-                title: "page2"
-            }
-        },
-        page3: {
-            screen: page3,
-            navigationOptions: {
-                title: "page3"
-            }
-        }
-    },
-    {
-        initialRouteName: 'TabNavigator'
-    }
-)
+const Stack = createStackNavigator();
 
-const AppStackContainer = createAppContainer(AppStack);
+function Home() {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
 
-export default class App extends Component {
-    render() {
-        return (
-            <AppStackContainer />
-        );
-    }
+                    if (route.name === 'Home') {
+                        iconName = focused
+                            ? 'ios-information-circle'
+                            : 'ios-information-circle-outline';
+                    } else if (route.name === 'My') {
+                        iconName = focused ? 'ios-list-box' : 'ios-list';
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="My" component={MyScreen} />
+            <Tab.Screen name="News" component={NewsScreen} />
+            <Tab.Screen name="Find" component={FindScreen} />
+        </Tab.Navigator>
+    );
 }
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="page1" component={Page1} />
+                <Stack.Screen name="page2" component={Page2} />
+                <Stack.Screen name="page3" component={Page3} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+// const TabNavigator = createBottomTabNavigator(
+//     {
+//         "首页": HomeScreen,
+//         "消息": NewsScreen,
+//         "发现": FindScreen,
+//         "我的": MyScreen
+//     },
+//     {
+//         defaultNavigationOptions: ({ navigation }) => ({
+//             tabBarIcon: ({ focused, horizontal, tintColor }) => {
+//                 const { routeName } = navigation.state;
+//                 let IconComponent = Ionicons;
+//                 let iconName;
+//                 if (routeName === '首页') {
+//                     iconName = `ios-home`;
+//                 } else if (routeName === '消息') {
+//                     iconName = `md-information-circle`;
+//                 } else if (routeName === '我的') {
+//                     iconName = `ios-man`;
+//                 } else {
+//                     iconName = `ios-folder`;
+//                 }
+
+//                 return <IconComponent name={iconName} size={25} color={tintColor} />;
+//             },
+//         }),
+//         tabBarOptions: {
+//             activeTintColor: 'tomato',
+//             inactiveTintColor: 'gray'
+//         },
+//         initialRouteName: '首页'
+//     }
+// );
+
+// const AppStack = createStackNavigator(
+//     {
+//         TabNavigator: {
+//             screen: TabNavigator,
+//             navigationOptions: {
+//                 header: null
+//             }
+//         },
+//         page1: {
+//             screen: page1,
+//             navigationOptions: {
+//                 title: "page1"
+//             }
+//         },
+//         page2: {
+//             screen: page2,
+//             navigationOptions: {
+//                 title: "page2"
+//             }
+//         },
+//         page3: {
+//             screen: page3,
+//             navigationOptions: {
+//                 title: "page3"
+//             }
+//         }
+//     },
+//     {
+//         initialRouteName: 'TabNavigator'
+//     }
+// )
+
+// const AppStackContainer = createAppContainer(AppStack);
+
+// export default class App extends Component {
+//     render() {
+//         return (
+//             <AppStackContainer />
+//         );
+//     }
+// }
--- src/FindScreen.js
+++ src/FindScreen.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
-import { createAppContainer, createMaterialTopTabNavigator } from 'react-navigation';//界面导航
 import Icon from 'react-native-vector-icons/Ionicons';
+// 4x
+//import { createAppContainer, createMaterialTopTabNavigator } from 'react-navigation';//界面导航
+// 5x
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';//界面导航
 import Header from "./common/Header";
+
+const Tab = createMaterialTopTabNavigator();
 
 export default class App extends Component {
     constructor(props) {
@@ -14,7 +19,16 @@ export default class App extends Component {
             //   适配iphoneX
             <SafeAreaView style={{ flex: 1 }}>
                 <Header />
-                <MaterialTopTabNavigator />
+                {/* 4x */}
+                {/* <MaterialTopTabNavigator /> */}
+                {/* 5x */}
+                <Tab.Navigator>
+                    <Tab.Screen name="萨摩" component={HomeScreen} />
+                    <Tab.Screen name="哈士奇" component={SettingsScreen} />
+                    <Tab.Screen name="拉布拉多" component={ProfileScreen} />
+                    <Tab.Screen name="阿拉斯加" component={FeedScreen} />
+                    <Tab.Screen name="柴犬" component={FeedScreen2} />
+                </Tab.Navigator>
             </SafeAreaView>
         );
     }
@@ -70,85 +84,85 @@ class FeedScreen2 extends Component {
     }
 }
 
-const MaterialTopTabNavigator = createAppContainer(createMaterialTopTabNavigator(
-    {
-        Home: {
-            screen: HomeScreen,
-            navigationOptions: {
-                tabBarLabel: '萨摩',
-                tabBarIcon: ({ tintColor }) => (
-                    <Icon name='ios-home' color={tintColor} size={24} />
-                )
-            }
-        },
-        Settings: {
-            screen: SettingsScreen,
-            navigationOptions: {
-                tabBarLabel: '哈士奇',
-                tabBarIcon: ({ tintColor }) => (
-                    <Icon name='ios-settings' color={tintColor} size={24} />
-                )
-            }
-        },
-        Profile: {
-            screen: ProfileScreen,
-            navigationOptions: {
-                tabBarLabel: '拉布拉多',
-                tabBarIcon: ({ tintColor }) => (
-                    <Icon name='ios-list-box' color={tintColor} size={24} />
-                )
-            }
-        },
-        Feed: {
-            screen: FeedScreen,
-            navigationOptions: {
-                tabBarLabel: '阿拉斯加',
-                tabBarIcon: ({ tintColor }) => (
-                    <Icon name='ios-microphone' color={tintColor} size={24} />
-                )
-            }
-        },
-        Feed2: {
-            screen: FeedScreen2,
-            navigationOptions: {
-                tabBarLabel: '柴犬',
-                tabBarIcon: ({ tintColor }) => (
-                    <Icon name='ios-microphone' color={tintColor} size={24} />
-                )
-            }
-        }
-    }, {
-    initialRouteName: 'Home',
-    // swipeEnabled: false, // 是否允许滑动切换tabs 默认是true
-    animationEnabled: false, // 点击tab label切换tab时是否开启动画 默认为true
-    // order: ['Settings', 'Home'],//定义tab顺序的routeNames数组
-    tabBarPosition: 'top', // tab bar显示的位置，默认是 'top'
-    tabBarOptions: {
-        tabStyle: {
-            width: 100
-        },
-        scrollEnabled: true,//是否支持 选项卡滚动，默认false
-        activeTintColor: '#d03a28',
-        inactiveTintColor: '#697077',
-        fontSize: 30,
-        style: {
-            backgroundColor: '#fbf1ef'
-            // borderTopWidth: 0.5,
-            // borderTopColor: 'grey',
-        },
-        //设置 indicator(tab下面的那条线)的样式
-        indicatorStyle: {
-            height: 2, // 不显示indicator
-            backgroundColor: '#d03a28'
-        },
-        //showIcon: true, // 是否显示图标, 默认为false
-        //showLabel: false, // 是否显示label
-        //文字的样式
-        labelStyle: {
-            fontSize: 16
-        }
-    }
-}));
+// const MaterialTopTabNavigator = createAppContainer(createMaterialTopTabNavigator(
+//     {
+//         Home: {
+//             screen: HomeScreen,
+//             navigationOptions: {
+//                 tabBarLabel: '萨摩',
+//                 tabBarIcon: ({ tintColor }) => (
+//                     <Icon name='ios-home' color={tintColor} size={24} />
+//                 )
+//             }
+//         },
+//         Settings: {
+//             screen: SettingsScreen,
+//             navigationOptions: {
+//                 tabBarLabel: '哈士奇',
+//                 tabBarIcon: ({ tintColor }) => (
+//                     <Icon name='ios-settings' color={tintColor} size={24} />
+//                 )
+//             }
+//         },
+//         Profile: {
+//             screen: ProfileScreen,
+//             navigationOptions: {
+//                 tabBarLabel: '拉布拉多',
+//                 tabBarIcon: ({ tintColor }) => (
+//                     <Icon name='ios-list-box' color={tintColor} size={24} />
+//                 )
+//             }
+//         },
+//         Feed: {
+//             screen: FeedScreen,
+//             navigationOptions: {
+//                 tabBarLabel: '阿拉斯加',
+//                 tabBarIcon: ({ tintColor }) => (
+//                     <Icon name='ios-microphone' color={tintColor} size={24} />
+//                 )
+//             }
+//         },
+//         Feed2: {
+//             screen: FeedScreen2,
+//             navigationOptions: {
+//                 tabBarLabel: '柴犬',
+//                 tabBarIcon: ({ tintColor }) => (
+//                     <Icon name='ios-microphone' color={tintColor} size={24} />
+//                 )
+//             }
+//         }
+//     }, {
+//     initialRouteName: 'Home',
+//     // swipeEnabled: false, // 是否允许滑动切换tabs 默认是true
+//     animationEnabled: false, // 点击tab label切换tab时是否开启动画 默认为true
+//     // order: ['Settings', 'Home'],//定义tab顺序的routeNames数组
+//     tabBarPosition: 'top', // tab bar显示的位置，默认是 'top'
+//     tabBarOptions: {
+//         tabStyle: {
+//             width: 100
+//         },
+//         scrollEnabled: true,//是否支持 选项卡滚动，默认false
+//         activeTintColor: '#d03a28',
+//         inactiveTintColor: '#697077',
+//         fontSize: 30,
+//         style: {
+//             backgroundColor: '#fbf1ef'
+//             // borderTopWidth: 0.5,
+//             // borderTopColor: 'grey',
+//         },
+//         //设置 indicator(tab下面的那条线)的样式
+//         indicatorStyle: {
+//             height: 2, // 不显示indicator
+//             backgroundColor: '#d03a28'
+//         },
+//         //showIcon: true, // 是否显示图标, 默认为false
+//         //showLabel: false, // 是否显示label
+//         //文字的样式
+//         labelStyle: {
+//             fontSize: 16
+//         }
+//     }
+// }));
 
 const styles = StyleSheet.create({
     container: {
